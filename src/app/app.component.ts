2c508ea5414148c838a074469232b437fa49600d
Fabian Portilla <fas_456@hotmail.com>
2022-09-09 08:13:12 -0500

    Add basic styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html'
+  // template: `` 
 })
 export class AppComponent {
-  title = 'bases';
+  public title: string = 'Counter app';
+}
+
+const a = () => {
+  return 'hello'
 }
