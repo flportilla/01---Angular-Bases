b56b04406a0264fee79e397d4609cdb9d0ed0747
Fabian Portilla <fas_456@hotmail.com>
2022-09-12 17:44:14 -0500

    Clean unused consts

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Character } from '../interfaces/dbz.interfaces';
 @Component({
     selector: 'app-main-page',
